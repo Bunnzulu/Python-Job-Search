7ea57d171b6cd0d8121eb329bdd069ef2e7c80c2
Nobody <bunnza2020@gmail.com>
2024-07-24 17:54:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
+from math import ceil
 
 
-keyword = "python"
+keyword = "python" #Job keyword you want to search
 Page = 1
+StartPage = 1
 
 
-url = f"https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=25&txtKeywords={keyword}&postWeek=60&searchType=personalizedSearch&actualTxtKeywords={keyword}&searchBy=0&rdoOperator=OR&pDate=I&sequence={Page}&startPage=1"
+url = f"https://www.timesjobs.com/candidate/job-search.html?from=submit&luceneResultSize=25&txtKeywords={keyword}&postWeek=60&searchType=personalizedSearch&actualTxtKeywords={keyword}&searchBy=0&rdoOperator=OR&pDate=I&sequence={Page}&startPage={StartPage}"
 result = requests.get(url).text
 
 doc = BeautifulSoup(result,"html.parser")
 
 Boxes = doc.find_all(class_ = "clearfix job-bx wht-shd-bx")
 
-for Box in Boxes:
-    print(f"Title: {Box.a.text.strip()}")
-    print(f"Company: {Box.find(class_="joblist-comp-name").text.strip()}")
-    print("-----------")
+Number_of_Jobs = int(doc.find(id = "totolResultCountsId").text)
+
+Pages = ceil(Number_of_Jobs/25)
+
+# for Box in Boxes:
+#     Job_Title = Box.a.text.strip()
+#     Company_Title = Box.find(class_="joblist-comp-name").text.strip()
+#     Job_Skills = Box.find(class_="srp-skills").text.replace(' ','').strip()
+#     Job_Link = Box.a["href"]
+#     if Box.find("span").text == "": Job_Location = "Not Stated"
+#     if Box.find("span").text != "": Job_Location = Box.find("span").text
+#     print(f"Title: {Job_Title}")
+#     print(f"Company: {Company_Title}")
+#     print(f"Keyskills: {Job_Skills}")
+#     print(f"Job Link: {Job_Link}")
+#     print(f"Location: {Job_Location}")
+#     print("-----------")
